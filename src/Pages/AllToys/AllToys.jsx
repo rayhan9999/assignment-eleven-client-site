9744c2da24fb08696698b004c316b85b29df6a64
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-05-23 20:16:50 +0600

    all toys added

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -57,11 +57,12 @@ const AllToys = () => {
         <table className="table table-compact w-full ">
           <thead>
             <tr>
-
+              <th>Sl</th>
+              <th>Toy Name</th>
+              <th>Seller Name</th>
               <th>Sub Category</th>
               <th>Price</th>
-              <th>Quantity</th>
-              <th>Details</th>
+
             </tr>
           </thead>
           <tbody>
